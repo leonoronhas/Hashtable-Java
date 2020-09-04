09e05a2acad52b48ff96557405239a32a41ccdfc
Leonardo <leosud099@gmail.com>
2020-09-04 12:42:08 -0600

    Initial commit

---
+++ src/Hashtables/MyHashtableArray.java
@@ -0,0 +1,23 @@
+package Hashtables;
+
+public class MyHashtableArray {
+
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 122);
+        Employee leoSantos = new Employee("Leo", "Santos", 2553);
+        Employee dougJhonson = new Employee("Doug", "Jonson", 288);
+        Employee guiNascimento = new Employee("Gui", "Nascimento", 744);
+
+        HashtableArray ht = new HashtableArray();
+
+        ht.put("Jane", janeJones);
+        ht.put("Leo", leoSantos);
+        ht.put("Doug", dougJhonson);
+        ht.put("Gui", guiNascimento);
+
+        //ht.printHashtable();
+
+        System.out.println("Retrieve key Leo: " + ht.get("Leo"));
+
+    }
+}
